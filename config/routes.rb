959c67595f7b2263b1425443ebb8e6f7edd6d6d5
moomitt <100453545+moomitt@users.noreply.github.com>
2022-08-14 03:33:18 +0000

    [11] deviseのルーティング　修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  devise_for :users, skip: [:registrations, :passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
