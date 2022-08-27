df1cca006ba0fe79b31dd367d2c5631833616ef2
moomitt <100453545+moomitt@users.noreply.github.com>
2022-08-27 03:17:28 +0000

    [26] 書籍　お気に入り解除　修正

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -14,7 +14,7 @@ class Public::BookmarksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:book_id])
-    bookmark = @book.bookmarks.new(user_id: current_user.id)
+    bookmark = @book.bookmarks.find_by(user_id: current_user.id)
     if bookmark.present?      #二度押しのエラー回避
       bookmark.destroy
       redirect_to request.referer
