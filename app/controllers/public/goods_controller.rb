fcb3efc81c9ff5e75d493ea7edf8cc89663d540d
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-08 10:46:33 +0000

    [30] いいね機能 非同期化

--- app/controllers/public/goods_controller.rb
+++ app/controllers/public/goods_controller.rb
@@ -1,17 +1,17 @@
 class Public::GoodsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:comment_id])
     good = Good.new
     good.user_id = current_user.id
-    good.comment_id = comment.id
+    good.comment_id = @comment.id
     good.save
   end
   
   def destroy
     @book = Book.find(params[:book_id])
-    comment = Comment.find(params[:comment_id])
-    good = current_user.goods.find_by(comment_id: comment.id)
+    @comment = Comment.find(params[:comment_id])
+    good = current_user.goods.find_by(comment_id: @comment.id)
     good.destroy
   end
   
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@ import "bootstrap";
 import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all'
 
+require("jquery")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
