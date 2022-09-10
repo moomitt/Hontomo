a3bc91c9c0277de13b31dc28c014dc29d342439c
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-10 02:47:25 +0000

    [12] トップページ（新着書籍・人気書籍）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,12 @@
 class Public::HomesController < ApplicationController
   def top
+    @popular_books = Book.find(Comment.group(:book_id).order('count(id) desc').pluck(:book_id))
+    @new_books = Book.order('id DESC').limit(4)
   end
-
+  
+  #Commentモデルのbook_idが同じものでまとめて
+  #それをcomment_idの多い順に並び替えて、book_idを取り出す
+  
   def about
   end
 end
