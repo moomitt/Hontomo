2220d0ccfcfec248b8c2ad8eaf8e1215fe5c3b3f
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-11 05:18:44 +0000

    [37] 管理者　会員情報編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,7 +12,9 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to admin_user_path(user.id)
   end
 
   def comment
