c45e8e1cfeef18b6b88613441ef17cfaaba4866c
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-12 07:04:34 +0000

    [40] 管理者　書籍詳細

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -4,6 +4,10 @@ class Admin::BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    items = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+    @item = items.first
+    @comments = Comment.where(book_id: @book.id).sort{|a,b| b.goods.size <=> a.goods.size}
   end
 
   def edit
