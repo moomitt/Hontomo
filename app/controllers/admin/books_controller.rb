eb5e19ebd85c19677de734ca1cc70724e51a98fe
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-12 07:52:50 +0000

    [41] 管理者　書籍編集(public側も修正)

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -11,11 +11,26 @@ class Admin::BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    items = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+    @item = items.first
+    if params[:tag]
+      Tag.create(name: params[:tag])
+      redirect_to edit_admin_book_path(@book.id)
+    end
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to admin_book_path(book.id)
   end
 
   def destroy
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:user_id, :name, :author, :series, :isbn, tag_ids: [])
   end
 end
