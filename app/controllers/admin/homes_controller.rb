4ded4f1130e85ad3ba6e0e598014e51e339b4751
moomitt <100453545+moomitt@users.noreply.github.com>
2022-09-13 04:28:42 +0000

    [42] 管理者　コメント一覧

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,8 @@
 class Admin::HomesController < ApplicationController
   def top
+    @comments = Comment.all
+    if params[:keyword]
+      redirect_to admin_comments_search_path
+    end
   end
 end
